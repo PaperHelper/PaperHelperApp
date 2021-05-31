1faffd9fae8433896dfcb5ceeea2822fe5b6d46c
excited-hyun <nahyun1127@naver.com>
2021-05-31 18:51:24 +0900

    MoreView Alignment change

--- PaperHelper/MoreView.swift
+++ PaperHelper/MoreView.swift
@@ -26,8 +26,9 @@ struct MoreView: View {
                         
                         Text("Conformer: Convolution-augmented Transformer for Speech Recognition")
                             .font(.system(size: 19, weight: .semibold))
-                            .multilineTextAlignment(.center).lineLimit(1)
+                            .lineLimit(1)
                             .frame(width: 350, height: 60)
+                            .multilineTextAlignment(.leading)
                         Text("Recurrent neural networks have been the defacto choice for ASR. While Transformers are good at modeling long-range global context, they are less capable to extract ﬁnegrained local feature patterns. In this work, we study how to organically combine convolutions with self-attention in ASR models.")
                             .font(.system(size: 14))
                             .multilineTextAlignment(.leading).lineLimit(7)
@@ -38,8 +39,10 @@ struct MoreView: View {
                 
                 NavigationLink(destination: Paper2ndView()){
                     VStack{
-                        Text("MTH-IDS: A Multi-Tiered Hybrid Intrusion Detection System for Internet of Vehicles").font(.system(size: 19, weight: .semibold)).multilineTextAlignment(.center).lineLimit(1)
+                        Text("MTH-IDS: A Multi-Tiered Hybrid Intrusion Detection System for Internet of Vehicles").font(.system(size: 19, weight: .semibold))
+                            .lineLimit(1)
                             .frame(width: 350, height: 60)
+                            .multilineTextAlignment(.leading)
                         
                         Text("Cyber threats may decrease the stability and robustness of IoV, as well as cause vehicle unavailability or trafﬁc accidents. Machine learning and data mining algorithms have been recognized as effective models to design IDSs. A multi-tiered hybrid intrusion detection system is proposed to identify known and zero-day cyber-attacks on both intra-vehicle and external networks.")
                             .font(.system(size: 14))
@@ -50,8 +53,10 @@ struct MoreView: View {
                 
                 NavigationLink(destination: Paper3rdView()){
                     VStack{
-                        Text("Social-IWSTCNN: A Social Interaction-Weighted Spatio-Temporal Convolutional Neural Network for Pedestrian Trajectory Prediction in Urban Traffic Scenarios").font(.system(size: 19, weight: .semibold)).multilineTextAlignment(.center).lineLimit(1)
+                        Text("Social-IWSTCNN: A Social Interaction-Weighted Spatio-Temporal Convolutional Neural Network for Pedestrian Trajectory Prediction in Urban Traffic Scenarios").font(.system(size: 19, weight: .semibold))
+                            .lineLimit(1)
                             .frame(width: 350, height: 60)
+                            .multilineTextAlignment(.leading)
                         Text("We train and evaluate our algorithm on Waymo Open Dataset because it contains more sufﬁcient urban trafﬁc scenarios than the previously used ETH and UCY datasets. The pedestrians are labeled by 3D bounding boxes on LiDAR data with their real-world center position and size. We use MLP to learn the social interaction weights, and use an aggregate function to extract the spatial and social interaction features.")
                             .font(.system(size: 14))
                             .multilineTextAlignment(.leading).lineLimit(7)
