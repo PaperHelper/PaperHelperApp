93d8596a2b051219f4a9f560fe9ca514343e3172
excited-hyun <nahyun1127@naver.com>
2021-05-31 13:40:38 +0900

    Initial Commit

---
+++ PaperHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaperHelper
+//
+//  Created by 신나현 on 2021/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
